2b55faa81ef481bdb78bfce6615cb9ac8ac6df17
Yurii Aleshchenko <ualeshenko@gmail.com>
2019-12-10 11:10:06 +0200

    update

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
-from flask import Flask
-from flask import Markup
-from flask import request, render_template
+from flask import Flask, redirect, url_for
+from flask import Markup, request, render_template
+import mysql.connector
+
+def mysql_table():
+    connect = mysql.connector.connect(
+      host="localhost",
+      user="dev",
+      passwd="fallout",
+      database="audit"
+    )
+
+    cur = connect.cursor()
+
+    cur.execute("SELECT * FROM hosts") # (hostname, users) VALUES (%s, %s)"
+    data = cur.fetchall()
+    return render_template('table_hosts.html', data=data)
+
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return redirect(url_for('login'))
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] == 'yurii' and request.form['password'] == 'fallout':
-            return 'Hello World' #log_the_user_in(request.form['username'])
+            return mysql_table()
         else:
             error = 'Invalid username/password'
-    return render_template('login.html', error=error)
+    return render_template('login.html', error=error) #mysql_table()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
